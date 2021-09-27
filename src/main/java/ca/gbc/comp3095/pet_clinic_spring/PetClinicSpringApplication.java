ee8943bd1a2c177977b229bc805787eb0794442b
Hamza Koc <hamzakooc@gmail.com>
2021-09-27 16:40:26 -0400

    first commit

---
+++ src/main/java/ca/gbc/comp3095/pet_clinic_spring/PetClinicSpringApplication.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.pet_clinic_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/gbc/comp3095/pet_clinic_spring/PetClinicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.pet_clinic_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
