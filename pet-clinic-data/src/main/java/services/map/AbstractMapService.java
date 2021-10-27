adbb4a0946d550ca81c2e947453e295b91ba46ad
Hamza Koc <hamzakooc@gmail.com>
2021-10-27 11:42:11 -0400

    Map done.  Closes #6

--- pet-clinic-data/src/main/java/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/services/map/AbstractMapService.java
@@ -1,6 +1,48 @@
 package services.map;
 
-public class AbstractMapService {
+import com.sun.xml.bind.v2.model.core.ID;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class AbstractMapService<T, ID> {
+
+    protected Map<ID,T> map = new HashMap<>();
+
+    Set<T> findAll(){
+
+        return new HashSet(map.values());
+
+    }
+
+    T findById(ID id){
+        return map.get(id);
+    }
+
+    T save(ID id, T object){
+        map.put(id, object);
+        return object;
+    }
+
+    void deleteById(ID id){
+
+        map.remove(id);
+    }
+
+
+    void delete(T object){
+        map.entrySet().removeIf(entery -> entery.getValue().equals(object));
+    }
+
+
+
+
+
+
+
+
 
 
 
